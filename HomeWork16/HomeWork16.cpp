2fefa40ee417705758a5b0c6fd4b6e629d5ae159
lDrak <88560935+KostyaSD@users.noreply.github.com>
2024-03-15 22:11:43 +0300

    add HomeWork

--- HomeWork16/HomeWork16.cpp
+++ HomeWork16/HomeWork16.cpp
@@ -1,20 +1,38 @@
-// HomeWork16.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <time.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	struct tm buf;
+	time_t t = time(NULL);
+	localtime_s(&buf, &t);
+	std::cout << "Day: " << buf.tm_mday << '\n';
+
+	const int N = 6;
+	int array[N][N];
+	for (int i = 0; i < N; i++) 
+	{
+		for (int j = 0; j < N; j++) 
+		{
+			array[i][j] = i + j;
+			std::cout << array[i][j] << " ";
+		}
+		std::cout << '\n';
+	}
+
+	for (int i = 0, count = 0; i < N; i++) 
+	{
+		if (buf.tm_mday % N == i) 
+		{
+			std::cout << "Line " << i + 1 << ": ";
+			for (int j = 0; j < N; j++) 
+			{
+				std::cout << array[i][j] << " ";
+				count += array[i][j];
+			}
+			std::cout << "= " << count;
+		}
+
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
